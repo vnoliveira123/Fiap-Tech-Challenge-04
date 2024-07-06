f753466ce9173cac2b80fcc408d0c9f6b614e87a
Wellington Porto <wellington.brito@bulkconsulting.com.br>
2024-07-06 12:13:06 -0300

    Comentários adicionados nas funções do script model_lstm.py

--- model_lstm/model_lstm.py
+++ model_lstm/model_lstm.py
@@ -19,6 +19,7 @@ def load_and_process_data(excel_path, data_corte):
     data_scaled = scaler.fit_transform(df[['Close']])
     return df, data_scaled, scaler
 
+# Função para criar sequências de dados para entrada no modelo LSTM
 def create_sequences(dados, sequence_length):
     X, y = [], []
     for i in range(len(dados) - sequence_length):
@@ -26,6 +27,7 @@ def create_sequences(dados, sequence_length):
         y.append(dados[i+sequence_length])
     return np.array(X), np.array(y)
 
+# Função para construir o modelo LSTM
 def build_lstm_model(sequence_length):
     model_lstm = Sequential([
         LSTM(350, input_shape=(sequence_length, 1)),
@@ -34,10 +36,12 @@ def build_lstm_model(sequence_length):
     model_lstm.compile(optimizer='adam', loss=tf.keras.losses.MeanSquaredError())
     return model_lstm
 
+# Função para treinar o modelo LSTM
 def train_lstm_model(model, X_train, y_train, epochs, batch_size):
     model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
     return model
 
+# Função para avaliar o modelo LSTM usando métricas de desempenho
 def evaluate_lstm_model(model, X_test, y_test, scaler):
     lstm_predictions = model.predict(X_test)
     r2_lstm = r2_score(y_test, lstm_predictions)
@@ -47,10 +51,12 @@ def evaluate_lstm_model(model, X_test, y_test, scaler):
     rmse_lstm = np.sqrt(mean_squared_error(y_test, lstm_predictions))
     return r2_lstm, mse_lstm, mae_lstm, mape_lstm, rmse_lstm
 
+# Função para salvar o modelo treinado e scaler
 def save_model_and_scaler(model, scaler, model_path, scaler_path):
     model.save(model_path)
     joblib.dump(scaler, scaler_path)
 
+# Função para carregar o modelo salvo e scaler
 def load_model_and_scaler(model_path, scaler_path):
     if not os.path.exists(model_path) or not os.path.exists(scaler_path):
         raise FileNotFoundError("Model or scaler file not found.")
@@ -58,6 +64,7 @@ def load_model_and_scaler(model_path, scaler_path):
     scaler = joblib.load(scaler_path)
     return model_lstm, scaler
 
+# Função para prever valores futuros com o modelo LSTM
 def predict(num_prediction, data_scaled, sequence_length):
     try:
         # Carregar o modelo e o escalador
@@ -94,6 +101,7 @@ def predict(num_prediction, data_scaled, sequence_length):
         print(f"Erro durante a previsão: {e}")
     return None
 
+# Função para gerar uma lista de datas para as previsões
 def predict_dates(num_prediction, data):
     last_date = data['Data'].iloc[-1]
     prediction_dates = pd.date_range(last_date, periods=num_prediction+1).tolist()
